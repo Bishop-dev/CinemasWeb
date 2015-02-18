d1796f9b1c1e920dfbcbca5c2375075aff10b979
Bishop-dev <alex.bishop@yandex.ru>
2015-02-18 09:50:13 +0200

    backbone collection view initialized

--- static/js/view.js
+++ static/js/view.js
@@ -9,5 +9,25 @@ App.Views.MovieView = Backbone.View.extend({
     // rendering - create content of tag using model
     render: function() {
         this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
+
+App.Views.MoviesView = Backbone.View.extend({
+    initialize: function() {
+        console.log('movies collection initialized');
+    },
+    tagName: 'ul',
+    render: function() {
+        // loop each movie in collection
+        this.collection.each(function(movie){
+            // create view for each movie in collection
+            var movieView = new App.Views.MovieView({model: movie});
+            // append view of item to view of collection
+            this.$el.append(movieView.render().el);
+            // use context 'this' of MoviesView
+            // anonymous function 'function(movie){' use WINDOW as default context
+        }, this);
+        return this;
     }
 });
