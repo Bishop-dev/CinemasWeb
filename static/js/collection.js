385adaf2234e034b637150e3ff1de7aa73474dc7
Bishop-dev <alex.bishop@yandex.ru>
2015-02-20 23:53:30 +0200

    added CSS - removed li point

--- static/js/collection.js
+++ static/js/collection.js
@@ -1,4 +1,12 @@
 App.Collections.MovieCollection = Backbone.Collection.extend({
     model : App.Models.Movie,
-    url: '/movies/start'
+    url: '/movies/start',
+    events : {
+        'then': 'show',
+        'fetch': 'show'
+    },
+    show: function() {
+        console.log(this.models);
+        console.log(this.collection);
+    }
 });
